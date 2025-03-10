5050ffd4e8375c5b85e8183395d464f1f428014e
kpalmer-byte <youremail@byupathway.edu>
2025-03-10 18:44:10 -0500

    Homework

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.Globalization;
 using System.Transactions;
 
@@ -6,11 +7,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter a list of numbers. Type 0 when finished. ");
+        List<int> numbers = new List<int>();
+        int total = 0;
+        float average = 0;
+        int number = -1;
+        int largest = 0;
 
-        while (number != 0) ;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
         {
-        string number = Console.ReadLine();
-           string 
+            Console.Write("Enter number: ");
+            string entry = Console.ReadLine();
+            number = int.Parse(entry);
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+
+        foreach (int num in numbers)
+        {
+            total += num;
+            if (num > largest)
+            {
+                largest = num;
+            }
         }
+
+        if (numbers.Count > 0)
+        {
+            average = (float)total / numbers.Count;
+        }
+
+        Console.WriteLine($"The total is: {total} The average is: {average} The largest number is {largest}");
+    }
 }
