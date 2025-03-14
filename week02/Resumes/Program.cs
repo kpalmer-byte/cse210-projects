5d8cdc13c124bcbd6f7565369d0413257480592f
kpalmer-byte <youremail@byupathway.edu>
2025-03-13 22:28:04 -0500

    Homewrok Completed

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2000;
+        job1._endYear = 2008;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Data Analyst";
+        job2._company = "Adobe";
+        job2._startYear = 2010;
+        job2._endYear = 2012;
+
+        job1.Display();
+        job2.Display();
+
+        Resume resume = new Resume();
+        resume._name = "Benjamin Walker";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
